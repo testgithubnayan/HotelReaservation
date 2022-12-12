c81e908c4641c3b09a86f45f161123c05746c06c
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-12 10:13:01 +0530

    Adding INTO LIst

---
+++ Main/HotelReservationService.java
@@ -0,0 +1,15 @@
+package Hotelreaservation;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class HotelReservationService {
+	List<Hotel> hotelList = new ArrayList<>();
+
+	public boolean addHotel(Hotel hotel) {
+		hotelList.add(hotel);
+		return true;
+	}
+
+}
