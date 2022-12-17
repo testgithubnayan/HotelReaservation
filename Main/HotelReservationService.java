e3ee0a1ee1c955823aa9b69ec8428a40b1f665e7
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-17 19:07:23 +0530

    Update HotelReservationService.java

--- Main/HotelReservationService.java
+++ Main/HotelReservationService.java
@@ -1,15 +1,48 @@
-package Hotelreaservation;
+package com.bridgelabz.hotelreservation;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 public class HotelReservationService {
-	List<Hotel> hotelList = new ArrayList<>();
+    static LocalDate inDate ,outDate;
+    List<Hotel> hotelList = new ArrayList<>();
 
-	public boolean addHotel(Hotel hotel) {
-		hotelList.add(hotel);
-		return true;
-	}
+    public boolean addHotel(Hotel hotel){
+        hotelList.add(hotel);
+        return true;
+    }
+    Scanner scr = new Scanner(System.in);
+    String checkInDate;
+    String checkOutDate;
 
+    public void enterDates(){
+	
+	    //THE DATE ESTIMATION DURING THE WITHIN RANGE .........
+        
+	System.out.println("Enter check In Date: eg(dd-mm-yy)");
+        checkInDate = scr.nextLine();
+        System.out.println("Enter check Out Date: eg(dd-mm-yy)");
+        checkOutDate = scr.nextLine();
+
+        inDate = LocalDate.of(Integer.parseInt(checkInDate.substring(5,9)),Integer.parseInt(checkInDate.substring(3,4)),Integer.parseInt(checkInDate.substring(0,2)));
+        outDate = LocalDate.of(Integer.parseInt(checkOutDate.substring(5,9)),Integer.parseInt(checkOutDate.substring(3,4)),Integer.parseInt(checkOutDate.substring(0,2))).plusDays(1);
+
+    }
+//FINDING THE CHEAPEST ONE HOTEL..........
+     Hotel cheapestHotel(){
+        enterDates();
+        long totalDays = ChronoUnit.DAYS.between(inDate, outDate);
+         calculateTotalCost(totalDays);
+         hotelList.sort(Comparator.comparing(Hotel::getTotalCost));
+         Hotel cheapRate = hotelList.get(0);
+
+         return cheapRate;
+     }
+//THE TOTAL COST METHOD ...............
+    void calculateTotalCost(long totalDays) {
+        hotelList.stream().forEach(x -> {
+            x.setTotalCost(totalDays * x.getRate());
+        });
+    }
 }
